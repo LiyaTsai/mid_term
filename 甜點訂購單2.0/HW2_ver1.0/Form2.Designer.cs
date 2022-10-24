e35253e709999719850ce85faf45ef121c771d41
LiyaTsai <116324888+LiyaTsai@users.noreply.github.com>
2022-10-25 00:55:03 +0800

    訂購單UI

---
+++ 甜點訂購單2.0/HW2_ver1.0/Form2.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace HW2_ver1._0
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.picbox_logo = new System.Windows.Forms.PictureBox();
+            this.lb_delivery = new System.Windows.Forms.Label();
+            this.lb_phone = new System.Windows.Forms.Label();
+            this.lb_name = new System.Windows.Forms.Label();
+            this.lb_package = new System.Windows.Forms.Label();
+            this.lbox_cartPuff = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lb_total = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_discount = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_order = new System.Windows.Forms.Button();
+            this.btn_discount = new System.Windows.Forms.Button();
+            this.btn_RemoveAll = new System.Windows.Forms.Button();
+            this.brtn_RemoveOne = new System.Windows.Forms.Button();
+            this.lbox_cartSweet = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbtn_linepay = new System.Windows.Forms.RadioButton();
+            this.rbtn_trans = new System.Windows.Forms.RadioButton();
+            this.rbtn_cash = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.picbox_logo)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic", 40F);
+            this.label1.Location = new System.Drawing.Point(117, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(495, 69);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "MOMO 甜點團購單";
+            // 
+            // picbox_logo
+            // 
+            this.picbox_logo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picbox_logo.BackgroundImage")));
+            this.picbox_logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.picbox_logo.Location = new System.Drawing.Point(27, 8);
+            this.picbox_logo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picbox_logo.Name = "picbox_logo";
+            this.picbox_logo.Size = new System.Drawing.Size(100, 100);
+            this.picbox_logo.TabIndex = 2;
+            this.picbox_logo.TabStop = false;
+            // 
+            // lb_delivery
+            // 
+            this.lb_delivery.AutoSize = true;
+            this.lb_delivery.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.lb_delivery.Location = new System.Drawing.Point(21, 157);
+            this.lb_delivery.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_delivery.Name = "lb_delivery";
+            this.lb_delivery.Size = new System.Drawing.Size(123, 35);
+            this.lb_delivery.TabIndex = 11;
+            this.lb_delivery.Text = "取貨方式";
+            // 
+            // lb_phone
+            // 
+            this.lb_phone.AutoSize = true;
+            this.lb_phone.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.lb_phone.Location = new System.Drawing.Point(341, 123);
+            this.lb_phone.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_phone.Name = "lb_phone";
+            this.lb_phone.Size = new System.Drawing.Size(69, 35);
+            this.lb_phone.TabIndex = 12;
+            this.lb_phone.Text = "手機";
+            // 
+            // lb_name
+            // 
+            this.lb_name.AutoSize = true;
+            this.lb_name.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.lb_name.Location = new System.Drawing.Point(21, 123);
+            this.lb_name.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_name.Name = "lb_name";
+            this.lb_name.Size = new System.Drawing.Size(69, 35);
+            this.lb_name.TabIndex = 13;
+            this.lb_name.Text = "姓名";
+            // 
+            // lb_package
+            // 
+            this.lb_package.AutoSize = true;
+            this.lb_package.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.lb_package.Location = new System.Drawing.Point(341, 157);
+            this.lb_package.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_package.Name = "lb_package";
+            this.lb_package.Size = new System.Drawing.Size(69, 35);
+            this.lb_package.TabIndex = 12;
+            this.lb_package.Text = "包裝";
+            // 
+            // lbox_cartPuff
+            // 
+            this.lbox_cartPuff.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.lbox_cartPuff.FormattingEnabled = true;
+            this.lbox_cartPuff.ItemHeight = 35;
+            this.lbox_cartPuff.Location = new System.Drawing.Point(27, 238);
+            this.lbox_cartPuff.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lbox_cartPuff.Name = "lbox_cartPuff";
+            this.lbox_cartPuff.Size = new System.Drawing.Size(604, 144);
+            this.lbox_cartPuff.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.label2.Location = new System.Drawing.Point(21, 202);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 35);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "選購項目";
+            // 
+            // lb_total
+            // 
+            this.lb_total.BackColor = System.Drawing.Color.LightSalmon;
+            this.lb_total.Font = new System.Drawing.Font("Yu Gothic", 30F);
+            this.lb_total.Location = new System.Drawing.Point(506, 689);
+            this.lb_total.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_total.Name = "lb_total";
+            this.lb_total.Size = new System.Drawing.Size(126, 53);
+            this.lb_total.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.label7.Location = new System.Drawing.Point(419, 703);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 35);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "總計 $";
+            // 
+            // txt_discount
+            // 
+            this.txt_discount.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.txt_discount.Location = new System.Drawing.Point(146, 636);
+            this.txt_discount.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_discount.Name = "txt_discount";
+            this.txt_discount.Size = new System.Drawing.Size(240, 50);
+            this.txt_discount.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.label3.Location = new System.Drawing.Point(48, 642);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 35);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "折扣碼";
+            // 
+            // btn_order
+            // 
+            this.btn_order.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_order.BackgroundImage")));
+            this.btn_order.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.btn_order.Location = new System.Drawing.Point(392, 757);
+            this.btn_order.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_order.Name = "btn_order";
+            this.btn_order.Size = new System.Drawing.Size(240, 53);
+            this.btn_order.TabIndex = 18;
+            this.btn_order.Text = "送出訂單";
+            this.btn_order.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_order.UseVisualStyleBackColor = true;
+            this.btn_order.Click += new System.EventHandler(this.btn_order_Click);
+            // 
+            // btn_discount
+            // 
+            this.btn_discount.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_discount.BackgroundImage")));
+            this.btn_discount.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.btn_discount.Location = new System.Drawing.Point(392, 633);
+            this.btn_discount.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_discount.Name = "btn_discount";
+            this.btn_discount.Size = new System.Drawing.Size(240, 53);
+            this.btn_discount.TabIndex = 18;
+            this.btn_discount.Text = "使用折扣碼";
+            this.btn_discount.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_discount.UseVisualStyleBackColor = true;
+            this.btn_discount.Click += new System.EventHandler(this.btn_discount_Click);
+            // 
+            // btn_RemoveAll
+            // 
+            this.btn_RemoveAll.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_RemoveAll.BackgroundImage")));
+            this.btn_RemoveAll.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.btn_RemoveAll.Location = new System.Drawing.Point(390, 577);
+            this.btn_RemoveAll.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_RemoveAll.Name = "btn_RemoveAll";
+            this.btn_RemoveAll.Size = new System.Drawing.Size(240, 53);
+            this.btn_RemoveAll.TabIndex = 18;
+            this.btn_RemoveAll.Text = "移除全部";
+            this.btn_RemoveAll.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_RemoveAll.UseVisualStyleBackColor = true;
+            this.btn_RemoveAll.Click += new System.EventHandler(this.btn_RemoveAll_Click);
+            // 
+            // brtn_RemoveOne
+            // 
+            this.brtn_RemoveOne.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("brtn_RemoveOne.BackgroundImage")));
+            this.brtn_RemoveOne.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.brtn_RemoveOne.Location = new System.Drawing.Point(146, 577);
+            this.brtn_RemoveOne.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.brtn_RemoveOne.Name = "brtn_RemoveOne";
+            this.brtn_RemoveOne.Size = new System.Drawing.Size(240, 53);
+            this.brtn_RemoveOne.TabIndex = 18;
+            this.brtn_RemoveOne.Text = "移除單筆";
+            this.brtn_RemoveOne.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.brtn_RemoveOne.UseVisualStyleBackColor = true;
+            this.brtn_RemoveOne.Click += new System.EventHandler(this.brtn_RemoveOne_Click);
+            // 
+            // lbox_cartSweet
+            // 
+            this.lbox_cartSweet.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.lbox_cartSweet.FormattingEnabled = true;
+            this.lbox_cartSweet.ItemHeight = 35;
+            this.lbox_cartSweet.Location = new System.Drawing.Point(27, 418);
+            this.lbox_cartSweet.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lbox_cartSweet.Name = "lbox_cartSweet";
+            this.lbox_cartSweet.Size = new System.Drawing.Size(604, 144);
+            this.lbox_cartSweet.TabIndex = 14;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbtn_cash);
+            this.groupBox1.Controls.Add(this.rbtn_trans);
+            this.groupBox1.Controls.Add(this.rbtn_linepay);
+            this.groupBox1.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.groupBox1.Location = new System.Drawing.Point(38, 725);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(348, 85);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "付款方式";
+            // 
+            // rbtn_linepay
+            // 
+            this.rbtn_linepay.AutoSize = true;
+            this.rbtn_linepay.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.rbtn_linepay.Location = new System.Drawing.Point(15, 40);
+            this.rbtn_linepay.Name = "rbtn_linepay";
+            this.rbtn_linepay.Size = new System.Drawing.Size(140, 39);
+            this.rbtn_linepay.TabIndex = 0;
+            this.rbtn_linepay.TabStop = true;
+            this.rbtn_linepay.Text = "Line Pay";
+            this.rbtn_linepay.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_trans
+            // 
+            this.rbtn_trans.AutoSize = true;
+            this.rbtn_trans.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.rbtn_trans.Location = new System.Drawing.Point(165, 40);
+            this.rbtn_trans.Name = "rbtn_trans";
+            this.rbtn_trans.Size = new System.Drawing.Size(87, 39);
+            this.rbtn_trans.TabIndex = 0;
+            this.rbtn_trans.TabStop = true;
+            this.rbtn_trans.Text = "匯款";
+            this.rbtn_trans.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_cash
+            // 
+            this.rbtn_cash.AutoSize = true;
+            this.rbtn_cash.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.rbtn_cash.Location = new System.Drawing.Point(258, 40);
+            this.rbtn_cash.Name = "rbtn_cash";
+            this.rbtn_cash.Size = new System.Drawing.Size(87, 39);
+            this.rbtn_cash.TabIndex = 0;
+            this.rbtn_cash.TabStop = true;
+            this.rbtn_cash.Text = "付現";
+            this.rbtn_cash.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(668, 824);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_discount);
+            this.Controls.Add(this.brtn_RemoveOne);
+            this.Controls.Add(this.btn_RemoveAll);
+            this.Controls.Add(this.btn_order);
+            this.Controls.Add(this.txt_discount);
+            this.Controls.Add(this.lb_total);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lbox_cartSweet);
+            this.Controls.Add(this.lbox_cartPuff);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lb_delivery);
+            this.Controls.Add(this.lb_package);
+            this.Controls.Add(this.lb_phone);
+            this.Controls.Add(this.lb_name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picbox_logo);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form2";
+            this.Text = "購物車";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picbox_logo)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picbox_logo;
+        private System.Windows.Forms.Label lb_delivery;
+        private System.Windows.Forms.Label lb_phone;
+        private System.Windows.Forms.Label lb_name;
+        private System.Windows.Forms.Label lb_package;
+        private System.Windows.Forms.ListBox lbox_cartPuff;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lb_total;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_discount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_order;
+        private System.Windows.Forms.Button btn_discount;
+        private System.Windows.Forms.Button btn_RemoveAll;
+        private System.Windows.Forms.Button brtn_RemoveOne;
+        private System.Windows.Forms.ListBox lbox_cartSweet;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbtn_linepay;
+        private System.Windows.Forms.RadioButton rbtn_trans;
+        private System.Windows.Forms.RadioButton rbtn_cash;
+    }
+}
---
+++ 甜點訂購單2.0/HW2_ver1.0/codes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System;
+
+namespace HW2_ver1
+{
+    internal  class codes
+    {//全域變數
+
+        public static int additionPurchase;
+        public static List<ArrayList> customerInfo = new List<ArrayList>();
+        public static List<ArrayList> BuySweets = new List<ArrayList>();
+        public static List<ArrayList> BuyPuffs = new List<ArrayList>();
+    }
+}
+
